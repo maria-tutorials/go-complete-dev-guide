48de9ac8cf07130450a80b7ec307ae8c9bafab56
Maria Inês Serra <maria.serra@talkdesk.com>
2020-09-03 19:42:22 +0100

    lesson 26 - saving data to file

--- cards/deck.go
+++ cards/deck.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"strings"
 )
 
@@ -37,4 +38,8 @@ func deal(d deck, handsize int) (deck, deck) {
 // aux function to turn deck to string
 func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
--- cards/main.go
+++ cards/main.go
@@ -1,10 +1,11 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := newDeck()
-	fmt.Println(cards.toString())
+
+	cards.saveToFile("my_cards")
+
+	//fmt.Println(cards.toString())
 
 	//hand, restOfCards := deal(cards, 2)
 	//hand.print()
