88a9be0324744269f8cfba1fc10527c97a575204
Maria Inês Serra <maria.serra@talkdesk.com>
2020-09-03 19:03:20 +0100

    lesson 20 - creating a new deck

--- cards/deck.go
+++ cards/deck.go
@@ -6,6 +6,23 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardTypes := []string{"Diamonds", "Hearts"}
+	cardValues := []string{"Ace", "Two", "Three"}
+
+	for _, ctype := range cardTypes {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+ctype)
+		}
+	}
+
+	return cards
+}
+
+//receiver function:
+// any variable of type deck gets access to print
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
