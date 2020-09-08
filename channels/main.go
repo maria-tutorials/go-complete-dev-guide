0938da2bea35f84d1a4470b03bee46def1cb79ca
Maria Inês Serra <maria.serra@talkdesk.com>
2020-09-08 14:24:26 +0100

    lesson 69 - printing site status

---
+++ channels/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{
+		"http://reddit.com",
+		"http://mariainesserra.com",
+		"http://golang.org",
+		"http://talkdesk.com",
+	}
+
+	for _, link := range links {
+		checkLink(link)
+	}
+}
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down :(")
+		return
+	}
+
+	fmt.Println(link, "is up :)")
+}
